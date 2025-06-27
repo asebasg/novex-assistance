599c132b36f7d18d97c33ccc8578c8eb3642233d
Sebastián Ospina <asebasg07@gmail.com>
2025-06-27 15:57:58 -0500

    Operaciones CRUD con la entidad Employee (modulos).

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { EmployeeService } from './employee.service';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  // Metodo post (crear un usuario)
+  @Post()
+  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeeService.create(createEmployeeDto);
+  }
+
+  // Metodo get all (obtener todos los usuarios)
+
+  @Get()
+  findAll() {
+    return this.employeeService.findAll();
+  }
+
+  // Metodo get (obtener un usuario por id)
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.employeeService.findOne(id);
+  }
+
+  // Metodo patch (actualizar un usuario)
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+  ) {
+    return this.employeeService.update(id, updateEmployeeDto);
+  }
+
+  // Metodo delete (elimina un usuario)
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.employeeService.remove(id);
+  }
+}
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  constructor(private prisma: PrismaService) {}
+
+  // Crear empleado
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    return this.prisma.employee.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  // Encontrar todos los empleados
+  async findAll() {
+    return this.prisma.employee.findMany({});
+  }
+
+  // Encontrar empleado por ID
+  async findOne(id: number) {
+    const employee = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    // Si no se encuentra al empleado
+    if (!employee) {
+      throw new NotFoundException(
+        `No se encontro ningun empleado con la ID proporcionada`,
+      );
+    }
+    return employee;
+  }
+
+  // Actualizar un usuario
+  async update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    const employee = await this.prisma.employee.findUnique({
+      where: { id },
+    });
+    if (!employee) {
+      throw new NotFoundException(
+        `No se encontro ningun empleado con la ID proporcionada`,
+      );
+    }
+    return this.prisma.employee.update({
+      where: { id },
+      data: updateEmployeeDto,
+    });
+  }
+
+  // Eliminar un usuario
+  async remove(id: number) {
+    const employee = await this.prisma.employee.findUnique({
+      where: { id },
+    });
+
+    if (!employee) {
+      throw new NotFoundException(
+        `No se encontro ningun empleado con la ID proporcioanda`,
+      );
+    }
+
+    return this.prisma.employee.delete({
+      where: { id },
+    });
+  }
+}
