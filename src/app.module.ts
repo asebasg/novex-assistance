0e68ba45fdac97f3c682b2cbd32bd8392cfa81dc
Sebastián Ospina <asebasg07@gmail.com>
2025-06-27 13:33:24 -0500

    Operaciones CRUD con la entidad user (modulos)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { EmployeeModule } from './employee/employee.module';
 import { AttendanceModule } from './attendance/attendance.module';
 import { ReportModule } from './report/report.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, EmployeeModule, AttendanceModule, ReportModule],
+  imports: [UserModule, EmployeeModule, AttendanceModule, ReportModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
